4b96584b8ec1f5f60deac3cec0784c827028497f
Shaun <scpour@gmail.com>
2022-12-25 20:39:10 -0500

    Continued to modify page contents

--- src/Pages/Catering.js
+++ src/Pages/Catering.js
@@ -1,7 +1,11 @@
 const Catering = () => {
     return(
-            <p>Catering</p>
-        
+            <div>
+            <h1>Catering Services</h1>
+            <p>For parties of over 10 people, please contact our corporate office regarding catering options.
+                While we offer a wide range of catering packages at multiple price points, we must have advance notice in order to be able to schedule these services appropriately.
+            </p>
+            </div>
     )
 }
 
